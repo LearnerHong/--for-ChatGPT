ef3b52072c848577355900deefaed139f5153daf
ZhihongXu <47109379+LearnerHong@users.noreply.github.com>
2023-04-26 10:02:59 +0800

    Create paper_polishing_prompt.user.js

---
+++ paper_polishing_prompt.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         论文润色提示词 for ChatGPT
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  在ChatGPT页面上提供论文润色提示词
+// @author       Your Name
+// @match        https://*/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/LearnerHong/Paper-polishing-for-ChatGPT/paper_polishing_prompt.user.js
+
+
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const inputSelector = '在此处添加输入框的选择器'; // 根据ChatGPT页面的实际情况填写
+
+    const prompts = {
+        '/version': 'Please provide multiple versions for reference.',
+        '/logical': 'Please help me analyze and optimize the logical structure of this argument to make it more convincing.',
+        '/consistency': 'Please read and polish the entire paper to ensure consistency and coherence.'
+    };
+
+    document.querySelector(inputSelector).addEventListener('input', (event) => {
+        const inputElement = event.target;
+        const inputValue = inputElement.value;
+
+        if (inputValue in prompts) {
+            inputElement.value = prompts[inputValue];
+            event.preventDefault();
+        }
+    });
+
+})();
