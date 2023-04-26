c200dd09a601d9a836f9a7040ab742ffe5afa215
ZhihongXu <47109379+LearnerHong@users.noreply.github.com>
2023-04-26 10:29:04 +0800

    Update paper_polishing_prompt.user.js

--- paper_polishing_prompt.user.js
+++ paper_polishing_prompt.user.js
@@ -8,31 +8,60 @@
 // @grant        none
 // @updateURL https://raw.githubusercontent.com/LearnerHong/Paper-polishing-for-ChatGPT/main/paper_polishing_prompt.user.js
 // @downloadURL https://raw.githubusercontent.com/LearnerHong/Paper-polishing-for-ChatGPT/main/paper_polishing_prompt
-
-
-
-
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    const inputSelector = 'input[type="text"], textarea';// 根据ChatGPT页面的实际情况填写
+    const inputSelector = 'input[type="text"], textarea';
 
-    const prompts = {
-        '/version': 'Please provide multiple versions for reference.',
-        '/logical': 'Please help me analyze and optimize the logical structure of this argument to make it more convincing.',
-        '/consistency': 'Please read and polish the entire paper to ensure consistency and coherence.'
+    const promptSets = {
+        interview: {
+            en: {
+                '/version': 'Please provide multiple versions for reference.',
+                '/logical': 'Please help me analyze and optimize the logical structure of this argument to make it more convincing.',
+                '/consistency': 'Please read and polish the entire paper to ensure consistency and coherence.',
+            },
+            zh: {
+                '/version': '请提供多个版本供参考。',
+                '/logical': '请帮助我分析和优化这个论点的逻辑结构，使其更具说服力。',
+                '/consistency': '请阅读并润色整篇论文，确保一致性和连贯性。',
+            },
+        },
+        writing: {
+            en: {
+                '/version': 'Please provide multiple versions for reference.',
+                '/logical': 'Please help me analyze and optimize the logical structure of this argument to make it more convincing.',
+                '/consistency': 'Please read and polish the entire paper to ensure consistency and coherence.',
+            },
+            zh: {
+                '/version': '请提供多个版本供参考。',
+                '/logical': '请帮助我分析和优化这个论点的逻辑结构，使其更具说服力。',
+                '/consistency': '请阅读并润色整篇论文，确保一致性和连贯性。',
+            },
+        },
     };
+
+    // 添加选择框到页面
+    const selectBox = document.createElement('select');
+    selectBox.id = 'prompt-category';
+    selectBox.innerHTML = `
+      <option value="interview-en">面试 (英文)</option>
+      <option value="interview-zh">面试 (中文)</option>
+      <option value="writing-en">写作 (英文)</option>
+      <option value="writing-zh">写作 (中文)</option>
+    `;
+    document.body.appendChild(selectBox);
 
     document.querySelector(inputSelector).addEventListener('input', (event) => {
         const inputElement = event.target;
         const inputValue = inputElement.value;
+        const selectedCategory = document.querySelector('#prompt-category').value;
+        const [category, language] = selectedCategory.split('-');
 
-        if (inputValue in prompts) {
-            inputElement.value = prompts[inputValue];
+        if (inputValue in promptSets[category][language]) {
+            inputElement.value = promptSets[category][language][inputValue];
             event.preventDefault();
         }
     });
-
 })();
