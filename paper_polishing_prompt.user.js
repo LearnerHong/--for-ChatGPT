c0ccca861e6fb2acc567789ec6d73bf6bced0e65
ZhihongXu <47109379+LearnerHong@users.noreply.github.com>
2023-04-26 11:11:18 +0800

    Update paper_polishing_prompt.user.js

--- paper_polishing_prompt.user.js
+++ paper_polishing_prompt.user.js
@@ -31,26 +31,26 @@
         const promptSets = {
             interview: {
                 zh: {
-                    '/回答': '你现在是一个有着多年经验的阿里巴巴Java高级工程师，如果你遇到下面这个面试问题，请问你怎么回答，请示范。',
-                    '/讲解': '你现在是一个有着多年经验的阿里巴巴Java高级工程师，请你由浅入深、用通俗易懂的语言给我讲讲这个知识点。',
+                    '：：回答': '你现在是一个有着多年经验的阿里巴巴Java高级工程师，如果你遇到下面这个面试问题，请问你怎么回答，请示范。',
+                    '：：讲解': '你现在是一个有着多年经验的阿里巴巴Java高级工程师，请你由浅入深、用通俗易懂的语言给我讲讲这个知识点。',
                     '/出题': '你现在是一个有着多年经验的阿里巴巴Java高级工程师，请你针对这个知识点，出几点可能的面试问题，并给出你认为出色的回答。'
                 },
                 en: {
-                    '/回答': 'You are now a senior Java engineer with many years of experience at Alibaba. If you encounter the following interview question, how would you answer it? Please demonstrate.',
-                    '/讲解': 'You are now a senior Java engineer with many years of experience at Alibaba. Please explain this concept to me in simple and easy-to-understand language, starting from the basics.',
-                    '/出题': 'You are now a senior Java engineer with many years of experience at Alibaba. Please come up with several interview questions based on this topic and provide what you consider to be excellent answers.'
+                    '：：回答': 'You are now a senior Java engineer with many years of experience at Alibaba. If you encounter the following interview question, how would you answer it? Please demonstrate.',
+                    '：：讲解': 'You are now a senior Java engineer with many years of experience at Alibaba. Please explain this concept to me in simple and easy-to-understand language, starting from the basics.',
+                    '：：出题': 'You are now a senior Java engineer with many years of experience at Alibaba. Please come up with several interview questions based on this topic and provide what you consider to be excellent answers.'
                 }
             },
             writing: {
                 zh: {
-                    '/版本': '请提供多个版本供参考。',
-                    '/逻辑': '请帮助分析并优化这个论点的逻辑结构，以使其更具说服力。',
-                    '/一致性': '请阅读并润色整篇论文，确保一致性和连贯性。'
+                    '：：版本': '请提供多个版本供参考。',
+                    '：：逻辑': '请帮助分析并优化这个论点的逻辑结构，以使其更具说服力。',
+                    '：：一致性': '请阅读并润色整篇论文，确保一致性和连贯性。'
                 },
                 en: {
-                    '/版本': 'Please provide multiple versions for reference.',
-                    '/逻辑': 'Please help me analyze and optimize the logical structure of this argument to make it more convincing.',
-                    '/一致': 'Please read and polish the entire paper to ensure consistency and coherence.'
+                    '：：版本': 'Please provide multiple versions for reference.',
+                    '：：逻辑': 'Please help me analyze and optimize the logical structure of this argument to make it more convincing.',
+                    '：：一致': 'Please read and polish the entire paper to ensure consistency and coherence.'
                 }
             }
         };
