061954b120c064783a37e238b743193e1492f503
ZhihongXu <47109379+LearnerHong@users.noreply.github.com>
2023-04-26 10:41:18 +0800

    Update paper_polishing_prompt.user.js

--- paper_polishing_prompt.user.js
+++ paper_polishing_prompt.user.js
@@ -5,72 +5,66 @@
 // @description  在ChatGPT页面上提供论文润色提示词
 // @author       Your Name
 // @match        https://*/*
-// @grant        none
-// @updateURL https://raw.githubusercontent.com/LearnerHong/Paper-polishing-for-ChatGPT/main/paper_polishing_prompt.user.js
-// @downloadURL https://raw.githubusercontent.com/LearnerHong/Paper-polishing-for-ChatGPT/main/paper_polishing_prompt
+// @grant        GM_addStyle
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
-    const inputSelector = 'input[type="text"], textarea';
+    const inputElement = document.querySelector('input[type="text"], textarea');
+    if (inputElement) {
+        GM_addStyle('.prompt-selector { position: absolute; top: 0; left: -200px; z-index: 1000; }');
 
-    const promptSets = {
-        interview: {
-            en: {
-                '/version': 'Please provide multiple versions for reference.',
-                '/logical': 'Please help me analyze and optimize the logical structure of this argument to make it more convincing.',
-                '/consistency': 'Please read and polish the entire paper to ensure consistency and coherence.',
-            },
-            zh: {
-                '/version': '请提供多个版本供参考。',
-                '/logical': '请帮助我分析和优化这个论点的逻辑结构，使其更具说服力。',
-                '/consistency': '请阅读并润色整篇论文，确保一致性和连贯性。',
-            },
-        },
-        writing: {
-            en: {
-                '/version': 'Please provide multiple versions for reference.',
-                '/logical': 'Please help me analyze and optimize the logical structure of this argument to make it more convincing.',
-                '/consistency': 'Please read and polish the entire paper to ensure consistency and coherence.',
-            },
-            zh: {
-                '/version': '请提供多个版本供参考。',
-                '/logical': '请帮助我分析和优化这个论点的逻辑结构，使其更具说服力。',
-                '/consistency': '请阅读并润色整篇论文，确保一致性和连贯性。',
-            },
-        },
-    };
+        const promptSelectorDiv = document.createElement('div');
+        promptSelectorDiv.className = 'prompt-selector';
+        inputElement.parentElement.appendChild(promptSelectorDiv);
 
-    // 添加选择框到页面
-    const selectBox = document.createElement('select');
-    selectBox.id = 'prompt-category';
-    selectBox.style.position = "fixed";
-    selectBox.style.top = "10px";
-    selectBox.style.left = "10px";
-    selectBox.innerHTML = `
-      <option value="interview-en">面试 (英文)</option>
-      <option value="interview-zh">面试 (中文)</option>
-      <option value="writing-en">写作 (英文)</option>
-      <option value="writing-zh">写作 (中文)</option>
-    `;
-    document.body.appendChild(selectBox);
+        const categorySelector = document.createElement('select');
+        categorySelector.innerHTML = '<option value="interview">面试</option><option value="writing">写作</option>';
+        promptSelectorDiv.appendChild(categorySelector);
 
-    const chatInputElement = document.querySelector(inputSelector);
+        const languageSelector = document.createElement('select');
+        languageSelector.innerHTML = '<option value="zh">中文</option><option value="en">英文</option>';
+        promptSelectorDiv.appendChild(languageSelector);
 
-    if(chatInputElement) {
-        chatInputElement.addEventListener('input', (event) => {
-            const inputElement = event.target;
-            const inputValue = inputElement.value;
-            const selectedCategory = document.querySelector('#prompt-category').value;
-            const [category, language] = selectedCategory.split('-');
+        const promptSets = {
+            interview: {
+                zh: {
+                    '/version': '请提供多个版本供参考。',
+                    '/logical': '请帮助分析并优化这个论点的逻辑结构，以使其更具说服力。',
+                    '/consistency': '请阅读并润色整篇论文，确保一致性和连贯性。'
+                },
+                en: {
+                    '/version': 'Please provide multiple versions for reference.',
+                    '/logical': 'Please help me analyze and optimize the logical structure of this argument to make it more convincing.',
+                    '/consistency': 'Please read and polish the entire paper to ensure consistency and coherence.'
+                }
+            },
+            writing: {
+                zh: {
+                    '/version': '请提供多个版本供参考。',
+                    '/logical': '请帮助分析并优化这个论点的逻辑结构，以使其更具说服力。',
+                    '/consistency': '请阅读并润色整篇论文，确保一致性和连贯性。'
+                },
+                en: {
+                    '/version': 'Please provide multiple versions for reference.',
+                    '/logical': 'Please help me analyze and optimize the logical structure of this argument to make it more convincing.',
+                    '/consistency': 'Please read and polish the entire paper to ensure consistency and coherence.'
+                }
+            }
+        };
+
+        inputElement.addEventListener('input', (event) => {
+            const category = categorySelector.value;
+            const language = languageSelector.value;
+            const inputValue = event.target.value;
 
             if (inputValue in promptSets[category][language]) {
-               inputElement.value = promptSets[category][language][inputValue];
-               event.preventDefault();
+                inputElement.value = promptSets[category][language][inputValue];
+                event.preventDefault();
             }
         });
     } else {
         console.error('Chat input element not found.');
     }
-    })();
+})();
